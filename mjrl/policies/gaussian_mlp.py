ec873ea72b3187bc280ea369a418d675d0fd7957
gunshi <gunshigupta@ymail.com>
2023-10-20 04:46:26 +0100

    added cnn policy

--- mjrl/policies/gaussian_mlp.py
+++ mjrl/policies/gaussian_mlp.py
@@ -203,3 +203,134 @@ class BatchNormMLP(MLP):
         # Placeholders
         # ------------------------
         self.obs_var = Variable(torch.randn(self.n), requires_grad=False)
+
+
+class BatchNormConvPoolMLP(torch.nn.Module):
+    def __init__(self, env_spec,
+                 hidden_sizes=(64,64),
+                 min_log_std=-3,
+                 init_log_std=0,
+                 seed=None,
+                 nonlinearity='relu',
+                 dropout=0,
+                 embedding_consolidation_type="sum",
+                 *args, **kwargs,
+                 ):
+        """
+        :param env_spec: specifications of the env (see utils/gym_env.py)
+        :param hidden_sizes: network hidden layer sizes (currently 2 layers only)
+        :param min_log_std: log_std is clamped at this value and can't go below
+        :param init_log_std: initial log standard deviation
+        :param seed: random seed
+        """
+        super(BatchNormConvPoolMLP, self).__init__()
+        self.history_window = 3
+        self.proprio_dim = 4
+        self.embed_channel_dim = 1280
+        self.reduced_channel_dim = 128
+        self.spatial_dims = 4
+        self.embedding_consolidation_type = embedding_consolidation_type
+
+        self.n = env_spec.observation_dim  # number of states
+        self.m = env_spec.action_dim  # number of actions
+        self.min_log_std = min_log_std
+        self.output_device = "cuda"
+
+        # Set seed
+        # ------------------------
+        if seed is not None:
+            torch.manual_seed(seed)
+            np.random.seed(seed)
+
+        # Policy network
+        # ------------------------
+        # make an input conv which keeps the same feature map shape and just reduces the channels from 1280 to 128
+        if self.embedding_consolidation_type == "3x3_conv" or self.embedding_consolidation_type == "1x1_conv":
+            if self.embedding_consolidation_type == "3x3_conv":
+                kernel_size = 3
+                padding = 1
+            elif self.embedding_consolidation_type == "1x1_conv":
+                kernel_size = 1
+                padding = 0
+            
+            input_conv = torch.nn.Conv2d(
+                in_channels=self.embed_channel_dim,
+                out_channels=self.reduced_channel_dim,
+                kernel_size=kernel_size,
+                stride=1,
+                padding=padding,
+            )
+            self.conv_layer = torch.nn.Sequential(
+                input_conv,
+                torch.nn.BatchNorm2d(num_features=128),
+                torch.nn.ReLU(True),
+                torch.nn.Flatten(),
+            )
+            reduced_observation_dim = self.reduced_channel_dim * (self.spatial_dims**2) * self.history_window + self.proprio_dim
+        else:
+            reduced_observation_dim = self.embed_channel_dim * self.history_window + self.proprio_dim
+            self.conv_layer = None
+
+        self.fc = FCNetworkWithBatchNorm(reduced_observation_dim, self.m, hidden_sizes, nonlinearity, dropout)
+
+
+        # make weights small
+        for param in list(self.fc.parameters())[-2:]:  # only last layer
+           param.data = 1e-2 * param.data
+        self.log_std = Variable(torch.ones(self.m) * init_log_std, requires_grad=True)
+        self.trainable_params = list(self.parameters()) + [self.log_std]
+        self.eval()
+
+        # Easy access variables
+        # -------------------------
+        self.log_std_val = np.float64(self.log_std.data.numpy().ravel())
+        self.param_shapes = [p.data.numpy().shape for p in self.trainable_params]
+        self.param_sizes = [p.data.numpy().size for p in self.trainable_params]
+        self.d = np.sum(self.param_sizes)  # total number of params
+
+        # Placeholders
+        # ------------------------
+        self.obs_var = Variable(torch.randn(self.n), requires_grad=False)
+
+
+    # Main functions
+    # ============================================
+    def get_action(self, observation):
+        o = np.float32(observation.reshape(1, -1))
+        self.obs_var.data = torch.from_numpy(o)
+        mean = self.forward(self.obs_var).data.numpy().ravel()
+        noise = np.exp(self.log_std_val) * np.random.randn(self.m)
+        action = mean + noise
+        return [action, {'mean': mean, 'log_std': self.log_std_val, 'evaluation': mean}]
+   
+    def forward(self, observations):
+        proprio_input = observations[:,-self.proprio_dim:]
+
+        if self.embedding_consolidation_type == "3x3_conv" or self.embedding_consolidation_type == "1x1_conv":
+            # first unwrap the observation which is the flattened spatial maps + prrprio features by removing the proprio dims and reshaping back to the original spatial map shape
+            # then pass through the conv layers
+            # then flatten the output and concatenate with the proprio features
+            # then pass through the fc layers then return the output
+            obs_input = observations[:,:-self.proprio_dim].reshape(observations.shape[0] * self.history_window, self.embed_channel_dim, self.spatial_dims, self.spatial_dims)
+            output = self.conv_layer(obs_input)
+        else:
+            obs_input = observations[:,:-self.proprio_dim].reshape(observations.shape[0], self.history_window * self.embed_channel_dim, self.spatial_dims, self.spatial_dims)
+            output = obs_input.mean(dim=[2, 3])
+
+        fc_input = torch.cat((output, proprio_input), dim=1)
+        fc_output = self.fc(fc_input)
+
+        return fc_output.to(self.output_device)
+
+    def to(self, device):
+        super().to(device)
+        if self.conv_layer is not None:
+            self.conv_layer.to(device)
+        self.fc.to(device)
+        self.obs_var.to(device)
+        self.output_device = device
+
+        return self
+    
+    def output_device_to(self, device):
+        self.output_device = device
